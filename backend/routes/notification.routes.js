6340af92cfe5763edca41105f883e45fb863294c
AAJB13 <115139237+AAJB13@users.noreply.github.com>
2025-03-20 04:04:55 +0800

    Delete backend directory

--- backend/routes/notification.routes.js
+++
@@ -1,8 +0,0 @@
-
-const router = require('express').Router();
-
-const { emailNotification
-} = require('../controllers/notification.controller');
-
-router.post('/email/:email', emailNotification);
-module.exports = router;
